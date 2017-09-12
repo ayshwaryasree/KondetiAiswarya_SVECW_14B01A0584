5262a5fdf8a5cd6cd71b2c4bc7e8227a3b9ea84e
ayshwaryasree <ayshwaryasree.ayshu@gmail.com>
2017-09-12 14:26:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hii");
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
