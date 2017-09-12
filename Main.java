3619983a83afdd417eec4797e7263fe06dc54f44
ayshwaryasree <ayshwaryasree.ayshu@gmail.com>
2017-09-12 14:35:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hii");
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
